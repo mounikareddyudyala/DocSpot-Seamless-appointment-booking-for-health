1e64edc8922e434119b7f6ea4f4c1a3628b1a1e0
lavanya8688049068 <lavanya8688049068@gmail.com>
2025-10-14 22:19:44 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -24,6 +24,8 @@ app.use('/api/doctor',require('./routes/doctorRoutes'))
 
 
 
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
